cca552bdd440a6ba0eb2e1cf04f19d8a218c2289
Maurilio Souza <1mauriliosouza@gmail.com>
2022-10-16 12:00:58 +0100

    RSEAT Lab DS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button"
+import "./styles/global.css"
+
+export function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl text-violet-500">Hello World dS</h1>
+      <Button>
+        Enviar
+      </Button>
+    </div>
+  )
+}
